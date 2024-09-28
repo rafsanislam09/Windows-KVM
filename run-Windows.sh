5969ed269c2c8c3edc4e297111cdea032135d1d4
Rafsan Islam <75265580+rafsanislam09@users.noreply.github.com>
2024-09-28 23:00:45 +0600

    Update run-Windows.sh

--- run-Windows.sh
+++ run-Windows.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 if [ -f virtio-win*.iso ]; then
     echo "A disk image of Virtio Drivers for Windows found. Skipping downloading virtio-win.iso."
 else
@@ -7,7 +8,7 @@ fi
 
 qemu-system-x86_64 \
  --enable-kvm \
- -cpu host \
+ -cpu host,hv-relaxed,hv-vapic,hv-vpindex,hv-runtime,hv-time,hv-synic,hv-stimer,hv-tlbflush,hv-ipi,hv-frequencies,hv-reenlightenment,hv-evmcs,hv-stimer-direct,hv-avic,hv-syndbg,hv-xmm-input,hv-tlbflush-ext,hv-tlbflush-direct \
  -machine q35 \
  -vga none -display gtk,gl=on \
  -m 2G \
@@ -15,13 +16,13 @@ qemu-system-x86_64 \
  -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE_4M.secboot.fd,index=0 \
  -drive if=pflash,format=raw,file=OVMF_VARS_4M.ms.fd,index=1 \
  -drive file=win.qcow2,id=disk,format=qcow2,index=2,if=none \
- -drive file=virtio-win.iso,format=raw,index=3,media=cdrom \
+ -drive file=virtio-win-0.1.262.iso,format=raw,index=3,media=cdrom \
+ -drive file="Windows.iso",format=raw,index=4,media=cdrom \
  -device virtio-scsi-pci,id=scsi \
- -device scsi-hd,drive=disk,vendor='RCA',product='RGB+++',serial='123456',wwn='6789',ver='2.5' \
+ -device scsi-hd,drive=disk,vendor='Rafsan',product='QCOW2 Disk',serial='0001',wwn='6789',ver='1.0' \
  -device virtio-balloon \
  -device usb-tablet \
  -device virtio-vga-gl,hostmem=16M \
  -audiodev pipewire,id=audio0 \
  -device intel-hda -device hda-output,audiodev=audio0 \
- -net nic,model=virtio \
- -usb \
+ -usb 
