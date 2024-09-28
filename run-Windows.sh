2c2d226970cb5f03599e185b87248d41629e92b9
Rafsan Islam <75265580+rafsanislam09@users.noreply.github.com>
2024-09-28 16:47:51 +0600

    Update run-Windows.sh

--- run-Windows.sh
+++ run-Windows.sh
@@ -1,3 +1,10 @@
+if [ -f virtio-win*.iso ]; then
+    echo "A disk image of Virtio Drivers for Windows found. Skipping downloading virtio-win.iso."
+else
+    echo "virtio-win.iso not found. Downloading...."
+    wget -O "virtio-win.iso" "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso"
+fi
+
 qemu-system-x86_64 \
  --enable-kvm \
  -cpu host \
@@ -8,7 +15,7 @@ qemu-system-x86_64 \
  -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE_4M.secboot.fd,index=0 \
  -drive if=pflash,format=raw,file=OVMF_VARS_4M.ms.fd,index=1 \
  -drive file=win.qcow2,id=disk,format=qcow2,index=2,if=none \
- -drive file=virtio-win-0.1.262.iso,format=raw,index=3,media=cdrom \
+ -drive file=virtio-win.iso,format=raw,index=3,media=cdrom \
  -device virtio-scsi-pci,id=scsi \
  -device scsi-hd,drive=disk,vendor='RCA',product='RGB+++',serial='123456',wwn='6789',ver='2.5' \
  -device virtio-balloon \
