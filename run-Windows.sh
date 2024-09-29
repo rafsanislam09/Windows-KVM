2c5a4fec09ab50b854c56d1c9b476a2b77855a9b
Rafsan Islam <75265580+rafsanislam09@users.noreply.github.com>
2024-09-29 17:49:43 +0600

    Update run-Windows.sh

--- run-Windows.sh
+++ run-Windows.sh
@@ -15,23 +15,28 @@ else
     qemu-img create -f qcow2 win.qcow2 $WINDOWS_DISK_CAPACITY
 fi
 
+#!/bin/sh
 qemu-system-x86_64 \
  --enable-kvm \
+ -name "Windows" \
  -cpu host,hv-relaxed,hv-vapic,hv-vpindex,hv-runtime,hv-time,hv-synic,hv-stimer,hv-tlbflush,hv-ipi,hv-frequencies,hv-reenlightenment,hv-evmcs,hv-stimer-direct,hv-avic,hv-syndbg,hv-xmm-input,hv-tlbflush-ext,hv-tlbflush-direct \
  -machine q35 \
  -vga none -display gtk,gl=on \
+ -netdev user,id=net0 \
  -m 2G \
  -smp 2 \
  -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE_4M.secboot.fd,index=0 \
  -drive if=pflash,format=raw,file=OVMF_VARS_4M.ms.fd,index=1 \
  -drive file=win.qcow2,id=disk,format=qcow2,index=2,if=none \
- -drive file=virtio-win-0.1.262.iso,format=raw,index=3,media=cdrom \
- -drive file="Windows.iso",format=raw,index=4,media=cdrom \
+ -drive file=virtio-win.iso,format=raw,index=3,media=cdrom \
  -device virtio-scsi-pci,id=scsi \
  -device scsi-hd,drive=disk,vendor='Rafsan',product='QCOW2 Disk',serial='0001',wwn='6789',ver='1.0' \
  -device virtio-balloon \
  -device usb-tablet \
+ -device virtio-net-pci,netdev=net0 \
  -device virtio-vga-gl,hostmem=16M \
- -audiodev pipewire,id=audio0 \
  -device intel-hda -device hda-output,audiodev=audio0 \
+ -audiodev sdl,id=audio0 \
+ -virtfs local,path=/home/hashem/,mount_tag=host0,security_model=passthrough,id=host0 \
  -usb 
+ 
