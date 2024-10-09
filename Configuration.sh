69c238368a13f2e6a0028648a85f8abb731a2ed2
rafsanislam09 <rafsan.islam010@gmail.com>
2024-10-09 16:16:55 +0600

    Update Configuration.sh

--- Configuration.sh
+++ Configuration.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Editable section. You can edit values here.
-export WINDOWS_DISK_CAPACITY=64G # Default maximum disk capacity is 64 Gigabyte. Change as your need. Remember, this value cannot be changed after the installation of Windows.
+export WINDOWS_DISK_CAPACITY=64G # Default maximum disk capacity is 64 Gigabyte. Change as your need. Remember, this value cannot be changed after installting Windows.
 export PROCESSOR_CORE_AMOUNT=2 # Default is 2 core. Add or remove core(s) as your need.
 export RAM=4G # Default is 4 Gigabyte RAM. Change as your need.
